ff8e71ad6d8cc1a9b76bff4380173482558b965b
Jubi Dition <yubi4dition@googlemail.com>
2013-02-28 15:24:19 +0100

    footer larevel 3.2.13

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -1,5 +1,5 @@
 	<footer>
-		<p>&copy; 2012 built with Laravel 3.2.7 and Twitter Bootstrap 2.3.0</p>
+		<p>&copy; 2012 built with Laravel 3.2.13 and Twitter Bootstrap 2.3.0</p>
 	</footer>
 </div><!-- /container -->
 </body>
