acdb28244722a701734e15c1673bf7974574390e
Jubi Dition <yubi4dition@googlemail.com>
2013-02-28 15:52:16 +0100

    tabs to spaces

--- application/models/bookings.php
+++ application/models/bookings.php
@@ -1,26 +1,26 @@
 <?php
 
 class Bookings {
-	
-	public static function book($uid, $tid)
-	{
-		return DB::table('bookings')->insert(
-			array('uid' => $uid, 'tid' => $tid));
-	}
+    
+    public static function book($uid, $tid)
+    {
+        return DB::table('bookings')->insert(
+            array('uid' => $uid, 'tid' => $tid));
+    }
 
-	public static function get($uid)
-	{
-		return DB::table('bookings')
-			->join('trips', 'bookings.tid', '=', 'trips.id')
-			->join('locations', 'trips.lid', '=', 'locations.id')
-			->where_uid($uid)
-			->order_by('day', 'asc')
-			->get(array('bookings.id', 'locations.name', 'locations.day', 'trips.title', 'trips.cost'));
-	}
+    public static function get($uid)
+    {
+        return DB::table('bookings')
+            ->join('trips', 'bookings.tid', '=', 'trips.id')
+            ->join('locations', 'trips.lid', '=', 'locations.id')
+            ->where_uid($uid)
+            ->order_by('day', 'asc')
+            ->get(array('bookings.id', 'locations.name', 'locations.day', 'trips.title', 'trips.cost'));
+    }
 
-	public static function delete($booked_trip)
-	{
-		return DB::table('bookings')->where_id($booked_trip)->delete();
-	}
+    public static function delete($booked_trip)
+    {
+        return DB::table('bookings')->where_id($booked_trip)->delete();
+    }
 
 }
