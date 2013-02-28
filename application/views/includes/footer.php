0febbc4709d74887e08ba033564e9868d6d010ad
Jubi Dition <yubi4dition@googlemail.com>
2013-02-28 14:44:48 +0100

    bootstrap 2.3.0 - Responsive

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -1,5 +1,5 @@
 	<footer>
-		<p>&copy; 2012 built with Laravel 3.2.7 and Twitter Bootstrap 2.1.1</p>
+		<p>&copy; 2012 built with Laravel 3.2.7 and Twitter Bootstrap 2.3.0</p>
 	</footer>
 </div><!-- /container -->
 </body>
