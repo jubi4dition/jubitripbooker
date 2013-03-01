b66d2567301d322aaec959d07a0c129db5d2f3c2
Jubi Dition <yubi4dition@googlemail.com>
2013-03-01 01:57:06 +0100

    login form block-level

--- application/views/login/index.php
+++ application/views/login/index.php
@@ -8,10 +8,10 @@
   <div class="row">
   <div class="span4">
     <form class="well" action="<?=Url::to('login/check'); ?>" method="post" accept-charset="utf-8">
-      <input type="text" class="input-large" name="firstname" placeholder="Your Firstname" required maxlength="40" autofocus>
-      <input type="text" class="input-large" name="lastname" placeholder="Your Lastname" required maxlength="40">
-      <input type="text" class="input-large" name="number" placeholder="Your Number" required maxlength="6">
-      <button type="submit" class="btn btn-primary btn-large">
+      <input type="text" class="input-block-level" name="firstname" placeholder="Your Firstname" required maxlength="40" autofocus>
+      <input type="text" class="input-block-level" name="lastname" placeholder="Your Lastname" required maxlength="40">
+      <input type="text" class="input-block-level" name="number" placeholder="Your Number" required maxlength="6">
+      <button type="submit" class="btn btn-primary btn-block">
       <i class="icon-home icon-white"></i> Login</button>
     </form>
   </div>
