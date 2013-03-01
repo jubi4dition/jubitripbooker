247a19ff3585009c147456e0e28123d92b48c992
Jubi Dition <yubi4dition@googlemail.com>
2013-03-01 03:04:35 +0100

    footer update

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -1,5 +1,5 @@
 	<footer>
-		<p>&copy; 2012 built with Laravel 3.2.13 and Twitter Bootstrap 2.3.0</p>
+		<p>&copy; 2013 built with Laravel-3.2.13, Bootstrap-2.3.0 and jQuery-1.9.1</p>
 	</footer>
 </div><!-- /container -->
 </body>
