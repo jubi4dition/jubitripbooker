dbe4628abbcf7ff44d3fcc7c38ef204d78e6ea92
Jubi Dition <yubi4dition@googlemail.com>
2013-04-02 23:34:26 +0200

    new boxes admin main page

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -5,9 +5,25 @@
   <div class="page-header">
     <h2>Admin</h2>
   </div>
+  <h3>Welcome to the Admin Dashboard!</h3>
   <div class="row">
-  <div class="span4">
-    <p>You are on the Admin Page</p>
+  <div class="span3 offset1">
+    <div class="box box-primary">
+      <p>12</p>
+      <p>Users</p>
+    </div>
+  </div>
+  <div class="span3">
+    <div class="box box-warning">
+      <p>32</p>
+      <p>Trips</p>
+    </div>
+  </div>
+  <div class="span3">
+    <div class="box box-success">
+      <p>44</p>
+      <p>Bookings</p>
+    </div>
   </div>
   </div>
 </div>
